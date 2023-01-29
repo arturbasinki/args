205f0e9c25af692efda321b5ba32594ea5324977
airbass <artur.basinski@gmail.com>
2023-01-30 00:46:09 +0100

    implementing slice of arguments structs

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 type Argument struct {
 	Index int
 	Value string
 }
 
-func main() {
+var Arguments = []Argument{}
 
+func main() {
+	args := os.Args
+	for i, v := range args {
+		if i == 0 {
+			continue
+		}
+		temp := Argument{Index: i, Value: v}
+		Arguments = append(Arguments, temp)
+	}
+	fmt.Println(Arguments)
 }
